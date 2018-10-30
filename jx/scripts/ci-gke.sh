5ecf0672ccb89dac48751a8c7a062862c29effff
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-10-30 22:10:38 +0000

    feat: switch e2e tests to using prow based cluster

--- jx/scripts/ci-gke.sh
+++ jx/scripts/ci-gke.sh
@@ -8,18 +8,20 @@ gcloud auth activate-service-account --key-file $SERVICE_ACCOUNT_FILE
 git config --global --add user.name JenkinsXBot
 git config --global --add user.email jenkins-x@googlegroups.com
 
-
-jx create cluster gke -n ${CLUSTER_NAME,,} \
+jx create terraform -o ${CLUSTER_NAME,,} \
+    --cluster 'dev=gke' \
     --skip-login=true \
     --batch-mode \
     --labels kind=bdd,git=ghe \
-    --project-id ${PROJECT_ID} \
-    --zone ${ZONE} \
-    --machine-type n1-standard-2 \
-    --min-num-nodes=3 --max-num-nodes=5 \
-    --default-admin-password $JENKINS_PASSWORD \
+    --gke-project-id ${PROJECT_ID} \
+    --gke-zone ${ZONE} \
+    --gke-machine-type n1-standard-2 \
+    --gke-min-num-nodes=3 \
+    --gke-max-num-nodes=5 \
+    --default-admin-password ${JENKINS_PASSWORD} \
     --git-provider-url ${GIT_PROVIDER_URL} \
     --local-cloud-environment=true \
     --default-environment-prefix=b${BUILD_NUMBER} \
     --environment-git-owner=jenkins-x-tests
+
 jx namespace jx
